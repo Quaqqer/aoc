b410d3e503af3a312de2fbcdc17edc8f9d2cfc3a
Quaqqer <quaqqer@gmail.com>
2020-12-05 06:56:45 +0100

    Cleanup day 5

--- 2020/5/prg.py
+++ 2020/5/prg.py
@@ -6,26 +6,9 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2020, day=5);
 input = puzzle.input_data.split('\n')
 
-def getid(input):
-    miny = 0
-    maxy = 127
-    for i in range(7):
-        delta = maxy - miny
-        if input[i] == 'F':
-            maxy -= math.ceil(delta/2)
-        else:
-            miny += math.ceil(delta/2)
-    minx = 0
-    maxx = 7
-    for i in range(7, 10):
-        delta = maxx - minx
-        if input[i] == 'L':
-            maxx -= math.ceil(delta/2)
-        else:
-            minx += math.ceil(delta/2)
-
-    return maxy * 8 + minx
-
+def getid(input: str):
+    i = input.replace("B", "1").replace("F", "0").replace("R", "1").replace("L", "0")
+    return int(i, 2)
 
 ids = set()
 for i in input:
@@ -34,18 +17,6 @@ for i in input:
 print(f"Answer A: {max(ids)}")
 
 def isyours(id):
-    for i in range(0, id):
-        if i in ids:
-            break
-    else:
-        return False
-
-    for i in range(id + 1, 127 * 8 + 7):
-        if i in ids:
-            break
-    else:
-        return False
-
     return id not in ids and id -1 in ids and id + 1 in ids
 
 ans_b = -1
