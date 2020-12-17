c3ce0c6fa62889513f55b31c88b0a34d3205abaf
Quaqqer <quaqqer@gmail.com>
2020-12-17 06:29:01 +0100

    Finish day 17

--- 2020/17/prg.py
+++ 2020/17/prg.py
@@ -5,13 +5,18 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2020, day=17);
 input = puzzle.input_data.split("\n")
 
-grid = set()
-for y in range(len(input)):
-    for x in range(len(input[y])):
-        if input[y][x] == "#":
-            grid.add((x, y, 0))
+def creategrid(dims=3):
+    grid = set()
+    for y in range(len(input)):
+        for x in range(len(input[y])):
+            if input[y][x] == "#":
+                if dims == 3:
+                    grid.add((x, y, 0))
+                else:
+                    grid.add((x, y, 0, 0))
+    return grid
 
-def getneighbours(x, y, z):
+def getneighbours3(x, y, z):
     ret = set()
     for dx in range(-1, 2):
         for dy in range(-1, 2):
@@ -20,10 +25,10 @@ def getneighbours(x, y, z):
                     ret.add((x + dx, y + dy, z + dz))
     return ret
 
-def next(grid):
+def next3(grid):
     new = set()
     for node in grid:
-        neis = getneighbours(node[0], node[1], node[2])
+        neis = getneighbours3(node[0], node[1], node[2])
         aliveneis = 0
         for nei in neis:
             if nei in grid:
@@ -33,7 +38,7 @@ def next(grid):
 
         for nei in neis:
             if not nei in grid:
-                neineis = getneighbours(nei[0], nei[1], nei[2])
+                neineis = getneighbours3(nei[0], nei[1], nei[2])
                 aliveneineis = 0
                 for neinei in neineis:
                     if neinei in grid:
@@ -42,18 +47,46 @@ def next(grid):
                     new.add(nei)
     return new
 
-def prtgrid(grid, size):
-    d2 = [[False for i in range(size)] for i in range(size)]
-    for x in range(size):
-        for y in range(size):
-            if (x, y, 0) in grid:
-                d2[y][x] = True
-    print("________________________")
-    for y in d2:
-        print("".join(["#" if i else "." for i in y]))
+def getneighbours4(x, y, z, o):
+    ret = set()
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            for dz in range(-1, 2):
+                for do in range(-1, 2):
+                    if not dx == dy == dz == do == 0:
+                        ret.add((x + dx, y + dy, z + dz, o + do))
+    return ret
 
+def next4(grid):
+    new = set()
+    for node in grid:
+        neis = getneighbours4(node[0], node[1], node[2], node[3])
+        aliveneis = 0
+        for nei in neis:
+            if nei in grid:
+                aliveneis += 1
+        if 2 <= aliveneis <= 3:
+            new.add(node)
+
+        for nei in neis:
+            if not nei in grid:
+                neineis = getneighbours4(nei[0], nei[1], nei[2], nei[3])
+                aliveneineis = 0
+                for neinei in neineis:
+                    if neinei in grid:
+                        aliveneineis += 1
+                if aliveneineis == 3:
+                    new.add(nei)
+    return new
+
+grid3 = creategrid(3)
+grid4 = creategrid(4)
 for i in range(6):
-    prtgrid(grid, i + 3)
-    grid = next(grid)
+    grid3 = next3(grid3)
+    grid4 = next4(grid4)
 
-print(len(grid))
+silver = len(grid3)
+print(f"Silver: {silver}")
+
+gold = len(grid4)
+print(f"Gold: {gold}")
