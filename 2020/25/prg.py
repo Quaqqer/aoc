bcccfb167c04962c055a9a60c38e31ce3d7ba513
Quaqqer <quaqqer@gmail.com>
2020-12-25 11:49:11 +0100

    Clean up day 25

--- 2020/25/prg.py
+++ 2020/25/prg.py
@@ -2,20 +2,10 @@
 
 from aocd.models import Puzzle
 
-puzzle = Puzzle(year=2020, day=25);
-input = [int(i) for i in puzzle.input_data.split("\n")]
-#input = [5764801, 17807724]
+puzzle = Puzzle(year=2020, day=25)
+p_card, p_door = (int(i) for i in puzzle.input_data.split("\n"))
 
-loop1 = 0
-v1 = 1
-while not v1 == input[0]:
-    v1 *= 7
-    v1 = v1 % 20201227
-    loop1 += 1
-
-v2 = input[1]
-v3 = 1
-for i in range(loop1):
-    v3 *= v2
-    v3 = v3 % 20201227
-print(f"Silver: {v3}")
+loops_card = 0
+while not pow(7, loops_card, 20201227) == p_card: loops_card += 1
+silver = pow(p_door, loops_card, 20201227)
+print(f"Silver: {silver}")
