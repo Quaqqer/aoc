32dec3d5d26114e3fab1e8683cad26e378f88bb7
Empa <emanuel@empa.xyz>
2021-12-05 21:46:27 +0100

    Reduce duplicate code.

--- 2021/5/prg.py
+++ 2021/5/prg.py
@@ -12,46 +12,36 @@ lines = puzzle.input_data.split("\n")
 
 regex = re.compile("([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)")
 
-intersects = defaultdict(int)
 
-for line in lines:
-    match = regex.fullmatch(line)
-    assert match
+def n_intersections(lines, skip_diagonals: bool):
+    intersects = defaultdict(int)
 
-    x1, y1, x2, y2 = tuple(int(v) for v in match.groups())
+    for line in lines:
+        match = regex.match(line)
+        assert match
 
-    if x1 == x2:
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            key = (x1, y)
-            intersects[key] += 1
-    elif y1 == y2:
-        for x in range(min(x1, x2), max(x1, x2) + 1):
-            key = (x, y1)
-            intersects[key] += 1
+        x1, y1, x2, y2 = tuple(int(v) for v in match.groups())
 
-silver = sum(map(lambda v: v >= 2, intersects.values()))
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                key = (x1, y)
+                intersects[key] += 1
+        else:
+            k = (y2 - y1) // (x2 - x1)
+            m = y1 - x1 * k
 
-intersects = defaultdict(int)
+            if skip_diagonals and k != 0:
+                continue
 
-for line in lines:
-    match = regex.match(line)
-    assert match
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                key = (x, k * x + m)
+                intersects[key] += 1
 
-    x1, y1, x2, y2 = tuple(int(v) for v in match.groups())
+    return sum(map(lambda v: v >= 2, intersects.values()))
 
-    if x1 == x2:
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            key = (x1, y)
-            intersects[key] += 1
-    else:
-        k = (y2 - y1) // (x2 - x1)
-        m = y1 - x1 * k
 
-        for x in range(min(x1, x2), max(x1, x2) + 1):
-            key = (x, k * x + m)
-            intersects[key] += 1
-
-gold = sum(map(lambda v: v >= 2, intersects.values()))
+silver = n_intersections(lines, skip_diagonals=True)
+gold = n_intersections(lines, skip_diagonals=False)
 
 # Print answers and send to aoc
 if "silver" in locals():
