2aa59f361521183b4cd7fbf34b5facc9bb31404a
Empa <emanuel@empa.xyz>
2021-12-05 06:39:12 +0100

    Some cleanup.

--- 2021/5/prg.py
+++ 2021/5/prg.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from aocd.models import Puzzle
 
-import re
-
 # Parse input
 puzzle = Puzzle(2021, 5)
 lines = puzzle.input_data.split("\n")
 
 # Main code
+
+import re
 
 regex = re.compile("([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)")
 
@@ -17,19 +17,16 @@ for line in lines:
     match = regex.match(line)
     assert match
 
-    x1, y1 = int(match.group(1)), int(match.group(2))
-    x2, y2 = int(match.group(3)), int(match.group(4))
+    x1, y1, x2, y2 = tuple(int(v) for v in match.groups())
 
     if x1 == x2:
         for y in range(min(y1, y2), max(y1, y2) + 1):
-            k = (x1, y)
-            if k not in intersects: intersects[k] = 0
-            intersects[k] += 1
+            key = (x1, y)
+            intersects[key] = intersects.get(key, 0) + 1
     elif y1 == y2:
         for x in range(min(x1, x2), max(x1, x2) + 1):
-            k = (x, y1)
-            if k not in intersects: intersects[k] = 0
-            intersects[k] += 1
+            key = (x, y1)
+            intersects[key] = intersects.get(key, 0) + 1
 
 silver = sum(map(lambda v: v >= 2, intersects.values()))
 
@@ -39,24 +36,19 @@ for line in lines:
     match = regex.match(line)
     assert match
 
-    x1, y1 = int(match.group(1)), int(match.group(2))
-    x2, y2 = int(match.group(3)), int(match.group(4))
+    x1, y1, x2, y2 = tuple(int(v) for v in match.groups())
 
     if x1 == x2:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             key = (x1, y)
-            if key not in intersects: intersects[key] = 0
-            intersects[key] += 1
+            intersects[key] = intersects.get(key, 0) + 1
     else:
         k = (y2 - y1) // (x2 - x1)
         m = y1 - x1 * k
 
         for x in range(min(x1, x2), max(x1, x2) + 1):
             key = (x, k*x + m)
-            if key not in intersects: intersects[key] = 0
-            intersects[key] += 1
-
-            pass
+            intersects[key] = intersects.get(key, 0) + 1
 
 gold = sum(map(lambda v: v >= 2, intersects.values()))
 
