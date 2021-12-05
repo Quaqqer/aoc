3920bfe0d9194b3a8107bc36d962531a78fc5cb0
Empa <emanuel@empa.xyz>
2021-12-05 06:44:44 +0100

    Use defaultdict, very nice!

--- 2021/5/prg.py
+++ 2021/5/prg.py
@@ -8,10 +8,11 @@ lines = puzzle.input_data.split("\n")
 # Main code
 
 import re
+from collections import defaultdict
 
 regex = re.compile("([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)")
 
-intersects = {}
+intersects = defaultdict(int)
 
 for line in lines:
     match = regex.match(line)
@@ -22,15 +23,15 @@ for line in lines:
     if x1 == x2:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             key = (x1, y)
-            intersects[key] = intersects.get(key, 0) + 1
+            intersects[key] += 1
     elif y1 == y2:
         for x in range(min(x1, x2), max(x1, x2) + 1):
             key = (x, y1)
-            intersects[key] = intersects.get(key, 0) + 1
+            intersects[key] += 1
 
 silver = sum(map(lambda v: v >= 2, intersects.values()))
 
-intersects = {}
+intersects = defaultdict(int)
 
 for line in lines:
     match = regex.match(line)
@@ -41,14 +42,14 @@ for line in lines:
     if x1 == x2:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             key = (x1, y)
-            intersects[key] = intersects.get(key, 0) + 1
+            intersects[key] += 1
     else:
         k = (y2 - y1) // (x2 - x1)
         m = y1 - x1 * k
 
         for x in range(min(x1, x2), max(x1, x2) + 1):
             key = (x, k*x + m)
-            intersects[key] = intersects.get(key, 0) + 1
+            intersects[key] += 1
 
 gold = sum(map(lambda v: v >= 2, intersects.values()))
 
