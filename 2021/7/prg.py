19b4153dcf9f406ff70d4e0562a9d9a6b510d4e3
Empa <emanuel@empa.xyz>
2021-12-07 10:39:28 +0100

    Use median and mean, I can't fully explain why it works though, I read that it does.

--- 2021/7/prg.py
+++ 2021/7/prg.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from aocd.models import Puzzle
 
 # Parse input
@@ -13,18 +14,32 @@ def cost_a(pos):
     return sum(abs(c - pos) for c in crabs)
 
 
-silver = min(cost_a(pos) for pos in range(min(crabs), max(crabs) + 1))
+def median(xs):
+    xs = sorted(xs)
+    if len(xs) % 2 == 0:
+        return xs[len(xs) // 2]
+    else:
+        mi = xs[(len(xs) // 2) - 1]
+        ma = xs[len(xs) // 2]
+        return (mi + ma) // 2
+
+
+silver = cost_a(median(crabs))
 
 
 def tri(n):
     return (n * (n + 1)) // 2
 
 
+def avg(xs):
+    return sum(xs) / len(xs)
+
+
 def cost_b(pos):
     return sum(tri(abs(c - pos)) for c in crabs)
 
 
-gold = min(cost_b(pos) for pos in range(min(crabs), max(crabs) + 1))
+gold = cost_b(int(avg(crabs)))
 
 # Print answers and send to aoc
 if "silver" in locals():
