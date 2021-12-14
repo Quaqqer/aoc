dbef65b17407166bc04d4d17b328cb524727c383
Empa <emanuel@empa.xyz>
2021-12-14 20:04:16 +0100

    Move days to day_x.

---
+++ 2021/bench.py
@@ -0,0 +1,26 @@
+from day_1 import prg
+from day_2 import prg
+from day_3 import prg
+from day_4 import prg
+from day_5 import prg
+from day_6 import prg
+from day_7 import prg
+from day_8 import prg
+from day_9 import prg
+from day_10 import prg
+from day_11 import prg
+from day_12 import prg
+from day_13 import prg
+
+# import day_14
+# import day_15
+# import day_16
+# import day_17
+# import day_18
+# import day_19
+# import day_20
+# import day_21
+# import day_22
+# import day_23
+# import day_24
+# import day_25
