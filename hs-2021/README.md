0c478c0a8340e86f696c32d22f7e18dc2a89459c
Empa <emanuel@empa.xyz>
2021-12-18 22:57:12 +0100

    Add day 1 solution in haskell.

---
+++ hs-2021/README.md
@@ -0,0 +1,4 @@
+# Haskell solutions for advent of code 2021
+To run feed puzzle input to stdin and run the haskell file with `runghc` for
+instance. I use aocd to get puzzle input, I would run `aocd 2021 1 | runghc
+day1.hs` for instance.
---
+++ hs-2021/day1.hs
@@ -0,0 +1,17 @@
+main :: IO ()
+main = do
+    contents <- getContents
+    let ds = depths contents
+    putStrLn $ "Silver: " ++ show (solveA ds)
+    putStrLn $ "Gold: " ++ show (solveB ds)
+
+depths :: String -> [Integer]
+depths i = map read (lines i)
+
+solveA :: [Integer] -> Integer
+solveA (x:y:ds) = (if y > x then 1 else 0) + solveA (y : ds)
+solveA _ = 0
+
+solveB :: [Integer] -> Integer
+solveB (a:b:c:d:rest) = (if a + b + c < b + c + d then 1 else 0) + solveB (b:c:d:rest)
+solveB _ = 0
