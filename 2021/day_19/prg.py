2beddf5212b63939cb3c9250435b7103c4ad0910
Empa <emanuel@empa.xyz>
2021-12-19 13:48:23 +0100

    Nicer manhattan distance.

--- 2021/day_19/prg.py
+++ 2021/day_19/prg.py
@@ -106,7 +106,7 @@ silver = len(all_beacons)
 
 
 def manhattan_distance(v1, v2):
-    return abs(v1[0] - v2[0]) + abs(v1[1] - v2[1]) + abs(v1[2] - v2[2])
+    return sum(abs(v1[i] - v2[i]) for i in range(len(v1)))
 
 
 gold = max(
