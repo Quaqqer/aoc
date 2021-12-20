91462d17e0b3e47ec09a1545aae7679528c977f4
Empa <emanuel@empa.xyz>
2021-12-21 00:08:26 +0100

    Finish day 20, ugly hardcode though.

--- 2021/day20.py
+++ 2021/day20.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 from aocd.models import Puzzle
 
+"""
+WARNING: Ugly hardcode, assuming that infinite grid toggles between on and off in input.
+"""
+
 # Parse input
 puzzle = Puzzle(2021, 20)
 algorithm, image_str = puzzle.input_data.split("\n\n")
@@ -8,27 +12,34 @@ algorithm, image_str = puzzle.input_data.split("\n\n")
 assert len(algorithm) == 512
 
 image_lines = image_str.splitlines()
-image = set()
+image = dict()
 for y in range(len(image_lines)):
     for x in range(len(image_lines[0])):
-        if image_lines[y][x] == "#":
-            image.add((x, y))
+        image[(x, y)] = image_lines[y][x] == "#"
+inf = False
 
 
 # Main code
-def _neighbins(image: set[tuple[int, int]], pos: tuple[int, int]) -> int:
+def _neighbins(
+    image: dict[tuple[int, int], bool], pos: tuple[int, int], inf: bool
+) -> int:
     bin = ""
     x, y = pos
     for dy in range(-1, 1 + 1):
         for dx in range(-1, 1 + 1):
             np = x + dx, y + dy
-            bin += "1" if np in image else "0"
+            if np in image:
+                bin += "1" if image[np] else "0"
+            else:
+                bin += "1" if inf else "0"
     return int(bin, 2)
 
 
-def apply(image: set[tuple[int, int]], algorithm: str) -> set[tuple[int, int]]:
+def apply(
+    image: dict[tuple[int, int], bool], inf: bool, algorithm: str
+) -> tuple[dict[tuple[int, int], bool], bool]:
     visited = set()
-    new_image = set()
+    new_image = dict()
 
     def update_neighbours(pos: tuple[int, int]):
         x, y = pos
@@ -37,36 +48,40 @@ def apply(image: set[tuple[int, int]], algorithm: str) -> set[tuple[int, int]]:
                 np = x + dx, y + dy
                 if np not in visited:
                     visited.add(np)
-                    if algorithm[_neighbins(image, np)] == "#":
-                        new_image.add(np)
+                    new_image[np] = algorithm[_neighbins(image, np, inf)] == "#"
 
     for x, y in image:
         update_neighbours((x, y))
 
-    return new_image
+    return new_image, not inf
 
 
-def draw(image: set[tuple[int, int]]):
-    xs = list(map(lambda v: v[0], image))
-    ys = list(map(lambda v: v[1], image))
+def draw(image: dict[tuple[int, int], bool], inf):
+    xs = list(map(lambda v: v[0], image.keys()))
+    ys = list(map(lambda v: v[1], image.keys()))
 
     min_x, max_x = min(xs), max(xs)
     min_y, max_y = min(ys), max(ys)
 
-    for y in range(min_y - 1, max_y + 2):
+    for y in range(min_y - 3, max_y + 4):
         line = []
-        for x in range(min_x - 1, max_x + 2):
-            line.append("#" if (x, y) in image else ".")
+        for x in range(min_x - 3, max_x + 4):
+            if (x, y) in image:
+                line.append("#" if image[(x, y)] else ".")
+            else:
+                line.append("#" if inf else ".")
         print("".join(line))
 
 
-print("Draw 1")
-draw(image)
-for i in range(2):
-    image = apply(image, algorithm)
-    print(f"Draw {i + 2}")
-    draw(image)
-silver = len(image)
+draw(image, inf)
+for _ in range(2):
+    image, inf = apply(image, inf, algorithm)
+silver = sum(image.values())
+
+for _ in range(50 - 2):
+    image, inf = apply(image, inf, algorithm)
+gold = sum(image.values())
+
 
 
 # Print answers and send to aoc
