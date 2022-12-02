29afb5d942675d4933a6368993039c548736019a
Empa <emanuel@empa.xyz>
2022-12-02 06:58:18 +0100

    Shorten substantially

--- 2022/day2.py
+++ 2022/day2.py
@@ -4,8 +4,14 @@ from aocd.models import Puzzle
 puzzle = Puzzle(2022, int("02"))
 id = puzzle.input_data
 
+
 # Main code
+def mod(a: int, b: int) -> int:
+    return ((a % b) + b) % b
+
+
 score_a = 0
+score_b = 0
 
 translation = {"X": "A", "Y": "B", "Z": "C"}
 
@@ -13,53 +19,27 @@ for line in id.splitlines():
     a, b_ = line.split(" ")
     b = translation[b_]
 
-    if b == "A":
-        score_a += 1
-    elif b == "B":
-        score_a += 2
-    elif b == "C":
-        score_a += 3
+    # score_a
+    # A => 1, B => 2, C => 3
+    score_a += ord(b) - 65 + 1
 
+    # Tie if they are the same
     if a == b:
         score_a += 3
-    elif a == "A" and b == "B":
+    # Win if b is the next character in the sequence [A, B, C] from a
+    elif b == chr(mod(ord(a) - 65 + 1, 3) + 65):
         score_a += 6
-    elif a == "B" and b == "C":
-        score_a += 6
-    elif a == "C" and b == "A":
-        score_a += 6
+
+    # score_b
+    if b == "A":
+        # Defeat, the next next character (+2)
+        score_b += 0 + mod(ord(a) - 65 + 2, 3) + 1
+    elif b == "B":
+        # Tie, the same charcter (+0)
+        score_b += 3 + mod(ord(a) - 65 + 0, 3) + 1
+    elif b == "C":
+        # Win, the next character (+1)
+        score_b += 6 + mod(ord(a) - 65 + 1, 3) + 1
 
 puzzle.answer_a = score_a
-
-score_b = 0
-
-for line in id.splitlines():
-    a, b_ = line.split(" ")
-    b = translation[b_]
-
-    if b == "A":
-        if a == "A":
-            score_b += 3
-        elif a == "B":
-            score_b += 1
-        elif a == "C":
-            score_b += 2
-        score_b += 0
-    elif b == "B":
-        if a == "A":
-            score_b += 1
-        elif a == "B":
-            score_b += 2
-        elif a == "C":
-            score_b += 3
-        score_b += 3
-    elif b == "C":
-        if a == "A":
-            score_b += 2
-        elif a == "B":
-            score_b += 3
-        elif a == "C":
-            score_b += 1
-        score_b += 6
-
 puzzle.answer_b = score_b
