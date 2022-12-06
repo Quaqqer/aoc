b0ae4bdcc1f0d5786f307c0b0825b708f490a7a0
Empa <emanuel@empa.xyz>
2022-12-06 06:24:58 +0100

    Nicer day 4

--- 2022/day6.py
+++ 2022/day6.py
@@ -4,25 +4,13 @@ from aocd.models import Puzzle
 puzzle = Puzzle(2022, int("06"))
 id = puzzle.input_data
 
-# Main code
-last_4 = []
-for i, char in enumerate(id):
-    last_4.append(char)
 
-    if len(last_4) > 4:
-        last_4.pop(0)
+def marker(n: int) -> int:
+    for i in range(len(id)):
+        if len(set(id[i : i + n])) == n:
+            return i + n
+    raise Exception("Should not fail")
 
-    if len(set(last_4)) == 4:
-        puzzle.answer_a = i + 1
-        break
 
-last_14 = []
-for i, char in enumerate(id):
-    last_14.append(char)
-
-    if len(last_14) > 14:
-        last_14.pop(0)
-
-    if len(set(last_14)) == 14:
-        puzzle.answer_b = i + 1
-        break
+puzzle.answer_a = marker(4)
+puzzle.answer_b = marker(14)
