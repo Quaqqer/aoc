40a5ae71265d8a5b9d2e02bf8c7e1928c8ffa3bc
Empa <emanuel@empa.xyz>
2023-12-21 23:03:54 +0100

    Shorten code substantially

--- 2023/day13.py
+++ 2023/day13.py
@@ -10,29 +10,18 @@ data = puzzle.input_data
 grids = [g.splitlines() for g in data.split("\n\n")]
 
 
-def reflection(l: Sequence[Sequence[str]], p2: bool) -> int | None:
-    # For every index i [1, len(l) - 1]
+def reflection(l: Sequence[Sequence[str]], p2: bool) -> int:
     for i in range(1, len(l)):
-        # slice a, b such that we get two slices of equal length on the
-        # opposite sides of i
-        length = min(i, len(l) - i)
-        a = tuple(l[i - length : i])
-        b = tuple(l[i : i + length])[::-1]
-
-        diffs = sum(1 for j in range(len(a)) for ac, bc in zip(a[j], b[j]) if ac != bc)
-
+        diffs = sum(
+            1 for a, b in zip(l[:i][::-1], l[i:]) for ac, bc in zip(a, b) if ac != bc
+        )
         if (p2 and diffs == 1) or (not p2 and diffs == 0):
             return i
+    return 0
 
 
 def solve(grids: list[list[str]], p2: bool) -> int:
-    s = 0
-    for g in grids:
-        if (hr := reflection(g, p2)) is not None:
-            s += 100 * hr
-        elif (vr := reflection(transpose(g), p2)) is not None:
-            s += vr
-    return s
+    return sum(reflection(g, p2) * 100 + reflection(transpose(g), p2) for g in grids)
 
 
 puzzle.answer_a = solve(grids, False)
