6c03c4fbd8a78a1b03388e3cd63069fc473dde97
Empa <emanuel@empa.xyz>
2023-12-24 08:30:21 +0100

    Forgot part 1 😬

--- 2023/day24.py
+++ 2023/day24.py
@@ -4,8 +4,7 @@
 # christmas eve and not christmas day. If there is another way I would love to
 # solve it myself. Merry christmas!
 
-from collections import namedtuple
-from typing import cast
+from typing import NamedTuple, cast
 
 import z3
 from aocd.models import Puzzle
@@ -16,21 +15,48 @@ puzzle = Puzzle(2023, int("24"))
 data = puzzle.input_data
 lines = data.splitlines()
 
-Vec3 = namedtuple("Vec3", ["x", "y", "z"])
 
-
-def to_k(dx: int, dy: int) -> float:
-    return dy / dx
-
-
-def to_m(x: int, y: int, k: int) -> float:
-    return y - x * k
+class Vec3(NamedTuple):
+    x: int
+    y: int
+    z: int
 
 
 hss: list[tuple[Vec3, Vec3]] = []
 for line in lines:
     x, y, z, dx, dy, dz = ints(line)
     hss.append((Vec3(x, y, z), Vec3(dx, dy, dz)))
+
+s = 0
+for i in range(len(hss)):
+    for j in range(i + 1, len(hss)):
+        ap, ad = hss[i]
+        apx, apy, apz = ap
+        adx, ady, adz = ad
+        bp, bd = hss[j]
+        bpx, bpy, bpz = bp
+        bdx, bdy, bdz = bd
+
+        ak = ady / adx
+        bk = bdy / bdx
+        am = apy - apx * ak
+        bm = bpy - bpx * bk
+
+        if ak - bk != 0:
+            ix = (bm - am) / (ak - bk)
+            iy = am + ix * ak
+
+            if (
+                (ix - apx) / adx > 0
+                and (ix - bpx) / bdx > 0
+                and (iy - apy) / ady > 0
+                and (iy - bpy) / bdy > 0
+                and 200000000000000 <= ix <= 400000000000000
+                and 200000000000000 <= iy <= 400000000000000
+            ):
+                s += 1
+
+puzzle.answer_a = s
 
 
 solver = z3.Solver()
