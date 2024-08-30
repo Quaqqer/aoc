15d497eefab1454a0e8b22bdfd05bccf8481446e
Empa <emanuel@empa.xyz>
2024-08-30 13:39:21 +0200

    Solve day 20 part 2, tedious

--- 2019/day20.py
+++ 2019/day20.py
@@ -1,22 +1,14 @@
-import functools
-import itertools
-import math
-import pprint
-import re
-from collections import Counter, defaultdict, deque
-from copy import copy, deepcopy
-from dataclasses import dataclass
+from collections import defaultdict, deque
 
-import util
 from aocd.models import Puzzle
-from util import Grid, Heap, Vec2, Vec3
+from util import Grid, Heap, Vec2
 
 puzzle = Puzzle(2019, int("20"))
 data = puzzle.input_data
 
 g = Grid.from_lines(data)
 
-portals: dict[str, list[Vec2[int]]] = defaultdict(list)
+portal_positions: dict[Vec2[int], str] = {}
 for x in range(g.cols):
     for y in range(g.rows):
         pos = Vec2(x, y)
@@ -32,54 +24,52 @@ for x in range(g.cols):
                 l = g[pos] + g[pos2]
 
                 if any(g[neigh] == "." for neigh in g.neighbours4(pos.tup)):
-                    portals[l].append(pos)
+                    portal_positions[pos] = l
                     g[pos] = "o"
                 else:
                     g[pos] = " "
 
                 if any(g[neigh] == "." for neigh in g.neighbours4(pos2.tup)):
-                    portals[l].append(pos2)
+                    portal_positions[pos2] = l
                     g[pos2] = "o"
                 else:
                     g[pos2] = " "
-portal_positions: dict[Vec2[int], str] = {}
-for portal, poss in portals.items():
-    for pos in poss:
-        portal_positions[pos] = portal
 
 
-def find_distances(g: Grid[str], start: Vec2[int]) -> dict[str, int]:
-    visited = {start}
-
-    distances: dict[str, int] = {}
-
-    q = deque([(0, start)])
-
-    while q:
-        cost, pos = q.popleft()
-
-        for neigh in (pos + delta for delta in Vec2.delta_4()):
-            if neigh not in visited and g[neigh] in {".", "o"}:
-                if g[neigh] == "o":
-                    distances[portal_positions[neigh]] = cost
-
-                    continue
-
-                visited.add(neigh)
-                q.append((cost + 1, neigh))
-
-    return distances
+def is_outer(pos: Vec2[int]) -> bool:
+    return pos.x == 1 or pos.y == 1 or pos.x == g.cols - 2 or pos.y == g.rows - 2
 
 
-distances: dict[str, dict[str, int]] = defaultdict(dict)
-for pos, portal in portal_positions.items():
-    dists = find_distances(g, pos)
-    for dist, cost in dists.items():
-        distances[portal][dist] = cost
+def search_a() -> int:
+    def find_distances(g: Grid[str], start: Vec2[int]) -> dict[str, int]:
+        visited = {start}
 
+        distances: dict[str, int] = {}
 
-def dijkstra[T](start: T, end: T, edges: dict[T, dict[T, int]]) -> int:
-    q = Heap([(-1, start)])
+        q = deque([(0, start)])
+
+        while q:
+            cost, pos = q.popleft()
+
+            for neigh in (pos + delta for delta in Vec2.delta_4()):
+                if neigh not in visited and g[neigh] in {".", "o"}:
+                    if g[neigh] == "o":
+                        distances[portal_positions[neigh]] = cost
+
+                        continue
+
+                    visited.add(neigh)
+                    q.append((cost + 1, neigh))
+
+        return distances
+
+    distances: dict[str, dict[str, int]] = defaultdict(dict)
+    for pos, portal in portal_positions.items():
+        dists = find_distances(g, pos)
+        for dist, cost in dists.items():
+            distances[portal][dist] = cost
+
+    q = Heap([(-1, "AA")])
 
     visited = set()
 
@@ -91,13 +81,87 @@ def dijkstra[T](start: T, end: T, edges: dict[T, dict[T, int]]) -> int:
         else:
             visited.add(pos)
 
-        if pos == end:
+        if pos == "ZZ":
             return cost
 
-        for portal, portal_cost in edges[pos].items():
+        for portal, portal_cost in distances[pos].items():
             q.push((cost + portal_cost, portal))
 
-    raise Exception()
+    raise Exception("No solution found")
 
 
-puzzle.answer_a = dijkstra("AA", "ZZ", distances)
+def search_b() -> int:
+    def find_distances(start: Vec2[int]) -> dict[str, int]:
+        visited = {start}
+
+        distances: dict[str, int] = {}
+
+        q = deque([(0, start)])
+
+        while q:
+            cost, pos = q.popleft()
+
+            for neigh in (pos + delta for delta in Vec2.delta_4()):
+                if neigh not in visited and g[neigh] in {".", "o"}:
+                    if g[neigh] == "o":
+                        outer = (
+                            neigh.x == 1
+                            or neigh.y == 1
+                            or neigh.x == g.cols - 2
+                            or neigh.y == g.rows - 2
+                        )
+
+                        distances[
+                            portal_positions[neigh] + "_" + ("o" if outer else "i")
+                        ] = cost
+
+                        continue
+
+                    visited.add(neigh)
+                    q.append((cost + 1, neigh))
+
+        return distances
+
+    distances: dict[str, dict[str, int]] = {
+        f'{portal}_{"o" if is_outer(pos) else "i"}': find_distances(pos)
+        for pos, portal in portal_positions.items()
+    }
+
+    q = Heap([(-1, 0, "AA_o")])
+
+    visited = set()
+
+    def flip_portal(p: str) -> str:
+        if p.endswith("o"):
+            return p[:-1] + "i"
+        else:
+            return p[:-1] + "o"
+
+    while q:
+        cost, layer, pos = q.pop()
+
+        if pos in visited:
+            continue
+        else:
+            visited.add((pos, layer))
+
+        if pos == "ZZ_i":
+            return cost
+
+        for portal, portal_cost in distances[pos].items():
+            if layer == 0:
+                if portal.endswith("o") and portal != "ZZ_o":
+                    continue
+            elif layer != 0:
+                if portal in {"AA_o", "ZZ_o"}:
+                    continue
+
+            new_layer = layer + (1 if portal.endswith("i") else -1)
+
+            q.push((cost + portal_cost, new_layer, flip_portal(portal)))
+
+    raise Exception("No solution found")
+
+
+puzzle.answer_a = search_a()
+puzzle.answer_b = search_b()
