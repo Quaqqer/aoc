65a476406fc0bd4da2b873355c6642212e2512fa
Empa <emanuel@empa.xyz>
2024-08-30 14:09:11 +0200

    Oneliners <3

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,21 +1,9 @@
+from itertools import accumulate
+
 from aocd.models import Puzzle
 
 puzzle = Puzzle(2015, int("01"))
 data = puzzle.input_data
 
 puzzle.answer_a = sum(c == "(" for c in data) - sum(c == ")" for c in data)
-
-
-def find_b() -> int:
-    s = 0
-    for i, c in enumerate(data):
-        if c == "(":
-            s += 1
-        if c == ")":
-            s -= 1
-        if s == -1:
-            return i + 1
-    raise Exception("No solution")
-
-
-puzzle.answer_b = find_b()
+puzzle.answer_b = list(accumulate([1 if c == "(" else -1 for c in data])).index(-1) + 1
