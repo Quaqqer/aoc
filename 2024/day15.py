5243774ff96b6e59e13d09301fa271ed58393af4
Emanuel Särnhammar <emanuel@quaqqer.com>
2024-12-15 08:17:33 +0100

    Fine, rewrite day 15

--- 2024/day15.py
+++ 2024/day15.py
@@ -1,6 +1,6 @@
 # 15   00:15:59   540      0   01:03:12  1176      0
-# Tough day today! I was going to make a clean rewrite, but it's a weekend so I
-# don't think I will.
+# Tough day today! Grid problems with larger structures than a single tile are
+# quite annoying tbh.
 
 from aocd.models import Puzzle
 from util import Grid, Vec2
@@ -50,68 +50,39 @@ def solve_a(g: Grid[str], start: Vec2[int]):
     return sum(100 * y + x for x, y in g.find_value("O"))
 
 
+def push_box_b(g: Grid[str], box_pos: Vec2, dir: Vec2) -> bool:
+    if g[box_pos] == "]":
+        box_pos -= Vec2(1, 0)
+
+    l, r = box_pos, box_pos + Vec2(1, 0)
+
+    ok = True
+
+    if dir.y != 0:
+        if g[l + dir] == "#" or g[r + dir] == "#":
+            return False
+
+        # Check for box left sides to the left below, below and right below
+        # Us -> [
+        #      [[[
+        for delta in [Vec2(-1, 0), Vec2(0, 0), Vec2(1, 0)]:
+            if g[l + delta + dir] == "[":
+                ok &= push_box_b(g, l + delta + dir, dir)
+
+        g[l], g[r] = ".", "."
+        g[l + dir], g[r + dir] = "[", "]"
+    else:
+        if g[l + dir if dir.x == -1 else l + 2 * dir] == "#":
+            return False
+        if g[l + dir * 2] == "[":
+            ok &= push_box_b(g, l + dir * 2, dir)
+        g[l], g[r] = ".", "."
+        g[l + dir], g[r + dir] = "[", "]"
+
+    return ok
+
+
 def solve_b(g: Grid[str], start: Vec2[int]):
-    def can_push_box(box_pos: Vec2, dir: Vec2) -> bool:
-        if g[box_pos] == "]":
-            box_pos -= Vec2(1, 0)
-
-        can_move = True
-        if dir.y != 0:
-            if g[box_pos + dir] in "[]":
-                can_move &= can_push_box(box_pos + dir, dir)
-            if g[box_pos + dir + Vec2(1, 0)] == "[":  # ]
-                can_move &= can_push_box(box_pos + dir + Vec2(1, 0), dir)
-            if g[box_pos + dir] == "#" or g[box_pos + dir + Vec2(1, 0)] == "#":
-                can_move = False
-        else:
-            if dir.x == 1:
-                if g[box_pos + dir * 2] == "[":  # ]
-                    can_move &= can_push_box(box_pos + dir * 2, dir)
-                if g[box_pos + dir * 2] == "#":
-                    can_move = False
-            else:
-                if g[box_pos + dir] == "]":
-                    can_move &= can_push_box(box_pos + dir, dir)
-                if g[box_pos + dir] == "#":
-                    can_move = False
-        return can_move
-
-    def push_box(box_pos: Vec2, dir: Vec2):
-        if g[box_pos] == "]":
-            box_pos -= Vec2(1, 0)
-
-        if dir.y != 0:
-            if g[box_pos + dir] in "[]":
-                push_box(box_pos + dir, dir)
-            if g[box_pos + dir + Vec2(1, 0)] == "[":  # ]
-                push_box(box_pos + dir + Vec2(1, 0), dir)
-
-            g[box_pos + dir] = "["
-            g[box_pos + dir + Vec2(1, 0)] = "]"
-            g[box_pos] = "."
-            g[box_pos + Vec2(1, 0)] = "."
-        else:
-            if dir.x == 1:
-                if g[box_pos + dir * 2] == "[":  # ]
-                    push_box(box_pos + dir * 2, dir)
-                g[box_pos], g[box_pos + Vec2(1, 0)], g[box_pos + Vec2(2, 0)] = (
-                    ".",
-                    "[",
-                    "]",
-                )
-            elif dir.x == -1:
-                if g[box_pos + dir * 2] == "[":  # ]
-                    push_box(box_pos + dir * 2, dir)
-                (
-                    g[box_pos + Vec2(-1, 0)],
-                    g[box_pos],
-                    g[box_pos + Vec2(1, 0)],
-                ) = (
-                    "[",
-                    "]",
-                    ".",
-                )
-
     pos = start
     for instruction in instructions:
         dir = DIR_MAP[instruction]
@@ -121,9 +92,10 @@ def solve_b(g: Grid[str], start: Vec2[int]):
             pos = n
         elif g[n] == "#":
             continue
-        elif g[n] in ["[", "]"]:
-            if can_push_box(n, dir):
-                push_box(n, dir)
+        elif g[n] in "[]":
+            gc = g.copy()
+            if push_box_b(gc, n, dir):
+                g = gc
                 pos = n
     return sum(y * 100 + x for x, y in g.find_value("["))
 
