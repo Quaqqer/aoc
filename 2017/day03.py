bd8935483e90a5bb73b9328854d3f5d6965a3091
Emanuel Särnhammar <emanuel@quaqqer.com>
2025-08-15 16:24:44 +0200

    Solve 2017 day 3 part 2

--- 2017/day03.py
+++ 2017/day03.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 
 from aocd.models import Puzzle
 
-# Parse input
 puzzle = Puzzle(2017, int("03"))
-id = puzzle.input_data
+id = int(puzzle.input_data)
 
 
-# Main code
 def square_coords(square_nr: int) -> tuple[int, int]:
     (x, y) = (0, 0)
     (dx, dy) = (1, 0)
@@ -26,5 +25,29 @@ def square_coords(square_nr: int) -> tuple[int, int]:
         (dx, dy) = (dy, -dx)
 
 
-square_nr = int(id)
-puzzle.answer_a = sum(map(abs, list(square_coords(square_nr))))
+def b_first_larger_square(id: int) -> int:
+    (x, y) = (0, 0)
+    (dx, dy) = (1, 0)
+
+    squares: dict[tuple[int, int], int] = defaultdict(int)
+    squares[0, 0] = 1
+
+    i = 1
+    a = 1
+    while True:
+        for _ in range((a + 1) // 2):
+            squares[x, y] = sum(
+                squares[x + dx, y + dy] for dx in (-1, 0, 1) for dy in (-1, 0, 1)
+            )
+
+            if squares[x, y] > id:
+                return squares[x, y]
+            (x, y) = (x + dx, y + dy)
+            i += 1
+        a += 1
+
+        (dx, dy) = (dy, -dx)
+
+
+puzzle.answer_a = sum(map(abs, list(square_coords(id))))
+puzzle.answer_b = b_first_larger_square(id)
