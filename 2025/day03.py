09e80897b678d773d7c4a2c1cd258abd4bb7e334
Emanuel Särnhammar <emanuel@quaqqer.com>
2025-12-03 06:54:14 +0100

    feat: solve day 3 2025

---
+++ 2025/day03.py
@@ -0,0 +1,51 @@
+# pyright: basic
+
+import functools
+
+from aocd.models import Puzzle
+
+puzzle = Puzzle(2025, int("03"))
+
+
+def max_bat_greedy(s: str, digits: int) -> int:
+    current = ""
+
+    i = 0
+    while digits > 0:
+        j = max(range(i, len(s) - digits + 1), key=lambda j: s[j])
+        current += s[j]
+        i = j + 1
+        digits -= 1
+
+    return int(current)
+
+
+def max_bat_dp(s: str, digits: int) -> int:
+    @functools.cache
+    def inner(start: int, digits: int) -> int | None:
+        best_jolts = None
+
+        for i in range(start, len(s)):
+            rest = inner(i + 1, digits - 1) if digits > 1 else ""
+
+            if rest is None:
+                continue
+
+            jolts = int(s[i] + str(rest))
+
+            if best_jolts is None or best_jolts < jolts:
+                best_jolts = jolts
+
+        return best_jolts
+
+    jolts = inner(0, digits)
+    assert jolts is not None
+    return jolts
+
+
+def solve(input: str, digits: int):
+    return sum(max_bat_greedy(line, digits) for line in input.splitlines())
+
+
+puzzle.answer_a = solve(puzzle.input_data, 2)
+puzzle.answer_b = solve(puzzle.input_data, 12)
